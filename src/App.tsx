5030da2ef8ffed71e6bab6aa8588563192bc0e23
RenanArantes <malevo18@gmail.com>
2022-10-07 09:42:51 -0300

    styles: global, theme and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <h1>Github Blog</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Blog</h1>
+    </ThemeProvider>
+  )
 }
 
 export default App
